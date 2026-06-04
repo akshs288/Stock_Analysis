456ac944f624b96942cf30d0844696a6e15e057b
akshs288 <aksht098@gmail.com>
2026-06-04 23:50:59 +0530

    Data can be inserted

--- main.py
+++ main.py
@@ -35,32 +35,41 @@ if st.button("Done"):
             with colc:
                 st.write("Average closing price:")
                 st.write(f"₹{round(df['Close'].mean(),2)} rupees")
-
+            
+            st.write(" ")
+            st.write(" ")
+            st.write(" ")
+            st.write(" ")
+            # Showing volatility for a share
+            st.write("Volatility")
+            try:
+                main_c = mql.connect(host= "localhost",user="root", database = "Stock_analysis", password= "Akshs123")
+                if main_c.is_connected():
+                    cursor = main_c.cursor()
+                    cursor.execute("use Stock_analysis;")
+                    tabel_name = file_select.replace("-", "_").replace(".csv","").replace(" ","_")
+                    cursor.execute(f"DROP TABLE IF EXISTS `{tabel_name}`;")
+                    cursor.execute(f"create table `{tabel_name}` (Date DATETIME,Open FLOAT,High FLOAT,Low float,Close float,Adj_Close float, Volume INT)")
+                
+                    df["Date"] = pd.to_datetime(df["Date"], format="mixed", dayfirst=True).dt.strftime("%Y-%m-%d")                    
+                    query = f"""
+                    INSERT INTO `{tabel_name}` (`Date`, `Open`, `High`, `Low`, `Close`, `Adj_Close`, `Volume`)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s)
+                    """
+                    data = df[["Date", "Open", "High", "Low", "Close", "Adj Close", "Volume"]].values.tolist()
+                    cursor.executemany(query, data)
+                    
+                    
+                    main_c.commit()
+                        
+            except Exception as e:
+                    print("Any error occured -",e)
+            st.write(" ")
+            st.write(" ")
+            st.write(" ")
+            st.write(" ")
             # Price Charts
             st.write("Price Chart")
             fig = go.Figure(data = [go.Candlestick(x=df["Date"], open = df["Open"], close=df["Close"],high = df["High"],low=df["Low"])])
             st.plotly_chart(fig)
-            
-            st.subheader("Gains and losses:")
-            with cold:
-                try:
-                    main_c = mql.connect(host= "localhost",user="root", database = "Stock_analysis", password= "Akshs123")
-                    if main_c.is_connected():
-                        cursor = main_c.cursor()
-                        cursor.execute("use Stock_analysis;")
-                        tabel_name = file_select.replace("-", "_").replace(".csv","").replace(" ","_")
-                        cursor.execute(f"create table if not exists `{tabel_name}` (Date DATETIME,Open FLOAT,High FLOAT,Low float,Close float,Adj_Close float, Volume INT);")
-                        df["Date"] = pd.to_datetime(df["Date"], format="%d-%m-%Y")
-                        df["Date"] = df["Date"].dt.strftime("%Y-%m-%d")
-                        
-                        query = f"INSERT INTO `{tabel_name}` (`Date`, `Open`,`High`,`Low`, `Close`,`Adj_Close`, `Volume`) values (%s,%s,%s,%s,%s,%s,%s)"
-                        data = df[["Date", "Open", "High", "Low", "Close", "Adj_Close", "Volume"]].values.tolist()
-                        cursor.executemany(query, data)
-                        main_c.commit()
-                        print(data)
-                        
-                        
-                except Exception as e:
-                    print("Any error occured -",e)
-
                     
